4596bb5cd27bea916ff6c5d6ff3a0726e4105694
Hakan Aydın <hakan.aydin@bilgeadam.com>
2022-05-28 16:20:56 +0300

    Add project files.

---
+++ ShopApp.DataAccess/Concrete/EfCore/ShopContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopApp.DataAccess.Concrete.EfCore
+{
+    public class ShopContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-VL8MVB0\\MSSQLSERVER2019;Database=ShopDB;integrated security=true;");
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
